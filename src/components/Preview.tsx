be921c215d360e784eab3a66992a6debfb4bac56
reisene <kprinny@gmail.com>
2026-08-24 10:14:13 +0200

    fix(preview): use relative image source paths

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,17 +3,17 @@ import { Image } from 'react-bootstrap';
 const previewImg = [
   {
     id: 1,
-    src: '/preview1.png',
+    src: './preview1.png',
     alt: 'Editor page',
   },
   {
     id: 2,
-    src: '/preview2.png',
+    src: './preview2.png',
     alt: 'Preview Modal',
   },
   {
     id: 3,
-    src: '/preview3.png',
+    src: './preview3.png',
     alt: 'Export Modal',
   },
 ];
