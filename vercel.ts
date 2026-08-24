ae084db98220856ef1708ed894590ca269037618
DraugSköll <kprinny@gmail.com>
2026-08-24 14:54:41 +0200

    Update vercel.ts

--- vercel.ts
+++ vercel.ts
@@ -7,7 +7,6 @@ import type { VercelConfig } from '@vercel/config/v1';
 
 export const config: VercelConfig = {
   regions: ['fra1'], // For more information and available regions, view https://vercel.com/docs/regions
-  functionFailoverRegions: ['cdg1'],
   cleanUrls: true,
   framework: 'nextjs',
   installCommand: 'npm i',
