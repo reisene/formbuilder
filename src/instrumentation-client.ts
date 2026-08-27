51b7cfecc522b3dece5b2c87ec22042b23f2430c
reisene <kprinny@gmail.com>
2026-08-27 19:56:14 +0200

    feat(sentry): add session replay integration
    - Enable Sentry replay integration for client
    - Add Sentry environment variable templates

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -23,7 +23,11 @@ Sentry.init({
     Sentry.feedbackIntegration({
       colorScheme: 'system',
     }),
+    Sentry.replayIntegration(),
   ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
